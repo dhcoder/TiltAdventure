3177a3521622fb6c5f503850f426f8d908844d2e
dhcoder <dhcoder@gmail.com>
2014-06-28 14:34:08 -0700

    Initial commit - new libgdx project

---
+++ desktop/src/tiltadv/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tiltadv.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tiltadv.GdxApplication;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxApplication(), config);
+	}
+}
