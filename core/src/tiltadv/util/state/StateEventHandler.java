91b8813c1d7621f85d74976603027fbc8079c205
dhcoder <dhcoder@gmail.com>
2014-07-06 12:34:23 -0700

    Updated header comments for State*Handler classes with generic types.

--- core/src/tiltadv/util/state/StateEventHandler.java
+++ core/src/tiltadv/util/state/StateEventHandler.java
@@ -5,6 +5,9 @@ import tiltadv.util.lambda.Action3;
 
 /**
  * A method like {@link StateTransitionHandler} but doesn't return a state to transition into.
+ *
+ * @param <S> An enumeration type that represents the known states this machine can get into.
+ * @param <E> An enumeration type that represents the known events this machine can accept.
  */
 public interface StateEventHandler<S extends Enum, E extends Enum> extends Action3<S, E, Opt> {
 
--- core/src/tiltadv/util/state/StateTransitionHandler.java
+++ core/src/tiltadv/util/state/StateTransitionHandler.java
@@ -7,6 +7,7 @@ import tiltadv.util.lambda.Func3;
  * A method which handles a state machine's event transition, returning which state the machine should transition into.
  *
  * @param <S> An enumeration type that represents the known states this machine can get into.
+ * @param <E> An enumeration type that represents the known events this machine can accept.
  */
 public interface StateTransitionHandler<S extends Enum, E extends Enum> extends Func3<Opt<S>, S, E, Opt> {
 
