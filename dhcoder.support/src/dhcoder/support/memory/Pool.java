20792f6fcfce40759b67aeac84cd6f8e133317e6
dhcoder <dhcoder@gmail.com>
2014-07-19 22:45:24 -0700

    Updated Pool header comment

--- dhcoder.support/src/dhcoder/support/memory/Pool.java
+++ dhcoder.support/src/dhcoder/support/memory/Pool.java
@@ -10,7 +10,16 @@ import java.util.Stack;
 import static dhcoder.support.utils.StringUtils.format;
 
 /**
- * A class which manages a pool of pre-allocated objects so that you
+ * A class which manages a pool of pre-allocated objects so you can avoid thrashing Android's garbage collector when you
+ * want to make lots of small, temporary allocations.
+ * <p/>
+ * Pools are constructed with two callbacks, one which allocates a new instance of a class, and one which clears an
+ * instance of a class for re-use later. After that, just call {@link #grabNew()} and {@link #free(Object)}, and this
+ * class will take care of the rest!
+ * <p/>
+ * Be careful using pools. After you grab something from a pool, you have to remember to release it - and if anyone is
+ * still holding on to that reference after you release it, that's an error - they will soon find the reference reset
+ * underneath them.
  */
 public class Pool<T> {
 
@@ -24,8 +33,8 @@ public class Pool<T> {
     private static int DEFAULT_CAPACITY = 10;
     private final AllocateMethod<T> allocate;
     private final ResetMethod<T> reset;
-    private final Stack<T> freeItems;
-    private final List<T> usedItems;
+    private final Stack<T> freeItems = new Stack<T>();
+    private final List<T> usedItems = new ArrayList<T>();
     private final int capacity;
 
     public Pool(final AllocateMethod<T> allocate, final ResetMethod<T> reset) {
@@ -36,9 +45,6 @@ public class Pool<T> {
         this.allocate = allocate;
         this.reset = reset;
         this.capacity = capacity;
-        freeItems = new Stack<T>();
-        freeItems.ensureCapacity(capacity);
-        usedItems = new ArrayList<T>(capacity);
 
     }
 
