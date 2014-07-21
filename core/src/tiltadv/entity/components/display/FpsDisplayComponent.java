9e6a97fc6a3d58ef5a3ca2de308ec3c59157dcaa
David Herman <davidherman@google.com>
2014-07-21 15:31:36 -0700

    Revert "Using mutable ints and floats so that StringUtils.formatInto doesn't trigger any allocations"
    This reverts commit aa5a8d87c1b56b3588e7dcb28c9ed358a4e07071.

--- core/src/tiltadv/entity/components/display/FpsDisplayComponent.java
+++ core/src/tiltadv/entity/components/display/FpsDisplayComponent.java
@@ -4,28 +4,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Vector2;
-import dhcoder.support.memory.MutableInt;
 import tiltadv.entity.AbstractComponent;
 import tiltadv.entity.Entity;
 import tiltadv.entity.components.data.TransformComponent;
 
-import java.nio.CharBuffer;
-
 import static dhcoder.support.utils.StringUtils.format;
-import static dhcoder.support.utils.StringUtils.formatInto;
 
 /**
  * Simple utility class that shows the current framerate.
  */
 public class FpsDisplayComponent extends AbstractComponent {
 
-    private static final String FPS_LABEL_FORMAT = "FPS: {0d}";
-    private static final int ESTIMATED_LABEL_LENGTH = format(FPS_LABEL_FORMAT, 10000).length();
-
     private final BitmapFont font;
     private TransformComponent transformComponent;
-    private final MutableInt fpsValue = new MutableInt();
-    private final CharBuffer fpsLabel = CharBuffer.allocate(ESTIMATED_LABEL_LENGTH);
 
     public FpsDisplayComponent(final BitmapFont font) {
         this.font = font;
@@ -38,12 +29,9 @@ public class FpsDisplayComponent extends AbstractComponent {
 
     @Override
     public void render(final Batch batch) {
-        fpsValue.set(Gdx.graphics.getFramesPerSecond());
-        if (fpsValue.get() > 0) {
-            Vector2 translate = transformComponent.getTranslate();
-            formatInto(fpsLabel, FPS_LABEL_FORMAT, fpsValue);
-            fpsLabel.flip();
-            font.draw(batch, fpsLabel, translate.x, translate.y);
-        }
+        int fps = Gdx.graphics.getFramesPerSecond();
+        String fpsString = (fps > 0) ? Integer.toString(fps) : "--";
+        Vector2 translate = transformComponent.getTranslate();
+        font.draw(batch, format("FPS: {0}", fpsString), translate.x, translate.y);
     }
 }
