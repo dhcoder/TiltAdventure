089b0adc9cb23cd805d9782395eb95496973a683
David Herman <davidherman@google.com>
2014-07-21 15:42:39 -0700

    Updated FpsDisplayComponent to not call format so much

--- core/src/tiltadv/entity/components/display/FpsDisplayComponent.java
+++ core/src/tiltadv/entity/components/display/FpsDisplayComponent.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Vector2;
+import dhcoder.support.time.Duration;
 import tiltadv.entity.AbstractComponent;
 import tiltadv.entity.Entity;
 import tiltadv.entity.components.data.TransformComponent;
@@ -15,8 +16,12 @@ import static dhcoder.support.utils.StringUtils.format;
  */
 public class FpsDisplayComponent extends AbstractComponent {
 
+    private final static String FPS_LABEL_FORMAT = "FPS: {0}";
     private final BitmapFont font;
     private TransformComponent transformComponent;
+
+    private int lastFps;
+    private String fpsLabel;
 
     public FpsDisplayComponent(final BitmapFont font) {
         this.font = font;
@@ -25,13 +30,21 @@ public class FpsDisplayComponent extends AbstractComponent {
     @Override
     public void initialize(final Entity owner) {
         transformComponent = owner.requireComponent(TransformComponent.class);
+        fpsLabel = format(FPS_LABEL_FORMAT, "--");
+    }
+
+    @Override
+    public void update(final Duration elapsedTime) {
+        int fps = Gdx.graphics.getFramesPerSecond();
+        if (fps != lastFps) {
+            fpsLabel = format(FPS_LABEL_FORMAT, fps);
+            lastFps = fps;
+        }
     }
 
     @Override
     public void render(final Batch batch) {
-        int fps = Gdx.graphics.getFramesPerSecond();
-        String fpsString = (fps > 0) ? Integer.toString(fps) : "--";
         Vector2 translate = transformComponent.getTranslate();
-        font.draw(batch, format("FPS: {0}", fpsString), translate.x, translate.y);
+        font.draw(batch, fpsLabel, translate.x, translate.y);
     }
 }
