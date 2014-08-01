b7423ea80d4bb40c393c4935b45d10f986af1bd7
dhcoder <dhcoder@gmail.com>
2014-07-31 22:42:03 -0700

    Updated ShapeUtilsTest after merging from remote

--- dhcoder.support/test/dhcoder/support/utils/ShapeUtilsTest.java
+++ dhcoder.support/test/dhcoder/support/utils/ShapeUtilsTest.java
@@ -8,58 +8,57 @@ import static dhcoder.support.utils.ShapeUtils.testIntersection;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-/* Expecting to pull these changes down and merge from another remote CL
 public final class ShapeUtilsTest {
 
     @Test
     public void testCircleIntersectsWithCircle() {
         // Circle1: W, Circle2: E, overlaps
-        assertThat(testIntersection(new Circle(-2f, -2f, 3f), new Circle(2f, -2f, 2f)), equalTo(true));
+        assertThat(testIntersection(new Circle(3f), -2f, -2f, new Circle(2f), 2f, -2f), equalTo(true));
 
         // Circle1: N, Circle2: S, overlaps
-        assertThat(testIntersection(new Circle(-5f, 10f, 3f), new Circle(-5f, 2f, 5f)), equalTo(true));
+        assertThat(testIntersection(new Circle(4f), -5f, 10f, new Circle(5f), -5f, 2f), equalTo(true));
 
         // Circle1: NW, Circle2: SE, overlaps
-        assertThat(testIntersection(new Circle(10f, 10f, 3f), new Circle(15f, 9f, 4f)), equalTo(true));
+        assertThat(testIntersection(new Circle(3f), 10f, 10f, new Circle(4f), 15f, 9f), equalTo(true));
 
         // Circle1: NE, Circle2: SW, overlaps
-        assertThat(testIntersection(new Circle(10f, 10f, 4f), new Circle(6f, 6f, 3f)), equalTo(true));
+        assertThat(testIntersection(new Circle(4f), 10f, 10f, new Circle(3f), 6f, 6f), equalTo(true));
 
         // Circle1 contains Circle2
-        assertThat(testIntersection(new Circle(0f, 0f, 5f), new Circle(0f, 0f, 1f)), equalTo(true));
+        assertThat(testIntersection(new Circle(5f), 0f, 0f, new Circle(1f), 0f, 0f), equalTo(true));
 
         // Circle1: W, Circle2: E, doesn't overlap
-        assertThat(testIntersection(new Circle(-2f, -2f, 3f), new Circle(8f, -2f, 2f)), equalTo(false));
+        assertThat(testIntersection(new Circle(3f), -2f, -2f, new Circle(2f), 8f, -2f), equalTo(false));
 
         // Circle1: N, Circle2: S, doesn't overlap
-        assertThat(testIntersection(new Circle(-5f, 10f, 3f), new Circle(-5f, -4f, 5f)), equalTo(false));
+        assertThat(testIntersection(new Circle(3f), -5f, 10f, new Circle(5f), -5f, -4f), equalTo(false));
 
         // Circle1: NW, Circle2: SE, doesn't overlap
-        assertThat(testIntersection(new Circle(10f, 10f, 3f), new Circle(15f, 15f, 1f)), equalTo(false));
+        assertThat(testIntersection(new Circle(3f), 10f, 10f, new Circle(1f), 15f, 15f), equalTo(false));
 
         // Circle1: NE, Circle2: SW, doesn't overlap
-        assertThat(testIntersection(new Circle(10f, 10f, 3f), new Circle(5f, 5f, 1f)), equalTo(false));
+        assertThat(testIntersection(new Circle(3f), 10f, 10f, new Circle(1f), 5f, 5f), equalTo(false));
     }
 
     @Test
     public void testRectangleIntersectsWithRectangle() {
         // Rect1: W, Rect2: E, overlaps
-        assertThat(testIntersection(new Rectangle(-2f, 0f, 4f, 4f), new Rectangle(4f, 0f, 4f, 4f)), equalTo(true));
+        assertThat(testIntersection(new Rectangle(4f, 4f), 2f, 0f, new Rectangle(4f, 4f), 8f, 0f), equalTo(true));
 
         // Rect1: N, Rect2: S, overlaps
-        assertThat(testIntersection(new Rectangle(5f, 10f, 4f, 4f), new Rectangle(5f, 3f, 4f, 4f)), equalTo(true));
+        assertThat(testIntersection(new Rectangle(4f, 4f), 5f, 10f, new Rectangle(4f, 4f), 5f, 3f), equalTo(true));
 
         // Rect1: NW, Rect2: SE, overlaps
-        assertThat(testIntersection(new Rectangle(10f, 10f, 2f, 3f), new Rectangle(15f, 5f, 4f, 4f)), equalTo(true));
+        assertThat(testIntersection(new Rectangle(2f, 3f), 10f, 10f, new Rectangle(4f, 4f), 15f, 5f), equalTo(true));
 
         // Rect1: NE, Rect2: SW, overlaps
-        assertThat(testIntersection(new Rectangle(10f, 10f, 2f, 3f), new Rectangle(5f, 5f, 4f, 4f)), equalTo(true));
+        assertThat(testIntersection(new Rectangle(2f, 3f), 10f, 10f, new Rectangle(4f, 4f), 5f, 5f), equalTo(true));
 
         // Rect1 and Rect2 in plus configuration
-        assertThat(testIntersection(new Rectangle(5f, 5f, 5f, 2f), new Rectangle(6f, 4f, 1f, 5f)), equalTo(true));
+        assertThat(testIntersection(new Rectangle(5f, 2f), 5f, 5f, new Rectangle(1f, 5f), 6f, 4f), equalTo(true));
 
         // Rect1 contains Rect2
-        assertThat(testIntersection(new Rectangle(5f, 5f, 5f, 2f), new Rectangle(5f, 5f, 3f, 1f)), equalTo(true));
+        assertThat(testIntersection(new Rectangle(5f, 2f), 5f, 5f, new Rectangle(3f, 1f), 5f, 5f), equalTo(true));
     }
 
     @Test
@@ -67,4 +66,3 @@ public final class ShapeUtilsTest {
         // TODO: Stop being lazy and write these tests
     }
 }
-*/
