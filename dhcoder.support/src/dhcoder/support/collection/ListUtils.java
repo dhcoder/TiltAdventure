6263ebd6ab66860291fc48886c21f7715ce72be4
dhcoder <dhcoder@gmail.com>
2014-08-05 22:36:53 -0700

    Fixed issue in "swapToEndAndRemove" if two objects in the list had equal values

--- dhcoder.support/src/dhcoder/support/collection/ListUtils.java
+++ dhcoder.support/src/dhcoder/support/collection/ListUtils.java
@@ -12,13 +12,21 @@ public final class ListUtils {
      * @throws IllegalArgumentException if the specified item is not in the list.
      */
     public static <T> void swapToEndAndRemove(final List<T> list, final T item) {
-        int itemIndex = list.indexOf(item);
+        int itemIndex = -1;
+        int listSize = list.size();
+        for (int i = 0; i < listSize; ++i) {
+            if (list.get(i) == item) { // Use == and not indexOf so we get instance equality, not value equality
+                itemIndex = i;
+                break;
+            }
+        }
+
         if (itemIndex < 0) {
             throw new IllegalArgumentException("Trying to remove an item that's not in the list");
         }
 
         // Swap item to the end before removing (since removing from the end avoids shifting elements)
-        int lastIndex = list.size() - 1;
+        int lastIndex = listSize - 1;
         if (lastIndex > 0 && itemIndex != lastIndex) {
             T temp = list.get(lastIndex);
             list.set(lastIndex, list.get(itemIndex));
