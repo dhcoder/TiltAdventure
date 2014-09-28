e63c9fa873417afd8880b0a01fed0b571de2ca7f
dhcoder <dhcoder@gmail.com>
2014-09-27 22:47:18 -0700

    Global events now final

--- core/src/tiltadv/globals/Events.java
+++ core/src/tiltadv/globals/Events.java
@@ -6,6 +6,6 @@ import dhcoder.support.event.Event;
  * A collection of system-wide events that any component might want to listen to.
  */
 public final class Events {
-    public static Event onScreenTouchDown = new Event();
-    public static Event onScreenTouchUp = new Event();
+    public static final Event onScreenTouchDown = new Event();
+    public static final Event onScreenTouchUp = new Event();
 }
