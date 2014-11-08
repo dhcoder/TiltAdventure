9736d3836be3d8897e204cf7e2de9553c0397ba4
David Herman <dhcoder@gmail.com>
2014-11-08 13:15:00 -0800

    ArraySet updates following ArrayMap updates

--- dhcoder.support/src/dhcoder/support/collection/ArraySet.java
+++ dhcoder.support/src/dhcoder/support/collection/ArraySet.java
@@ -27,16 +27,31 @@ public final class ArraySet<E> {
 
     public boolean contains(final E element) { return internalMap.containsKey(element); }
 
+    /**
+     * Add the element, which must NOT exist in the set. Use {@link #putIf(Object)} if you don't need this
+     * requirement.
+     */
     public void put(final E element) { internalMap.put(element, null); }
 
     /**
-     * Remove the element, which MUST exist in the map. Use {@link #removeIf(Object)} if you don't need this
+     * Add the element, if it's not already in the set.
+     */
+    public void putIf(final E element) { internalMap.putOrReplace(element, null); }
+
+    /**
+     * Remove the element, which MUST exist in the set. Use {@link #removeIf(Object)} if you don't need this
      * requirement. This distinction can be useful to assert cases when you want to guarantee the element is in the set,
      * and it also better mimics the related {@link ArrayMap} class.
      */
     public void remove(final E element) { internalMap.remove(element); }
 
+    /**
+     * Remove the element, which may or may not exist in the set. Use {@link #remove(Object)} if you want to assert
+     * existence of the element in the set.
+     */
     public void removeIf(final E element) { internalMap.removeIf(element); }
+
+
 
     public void clear() { internalMap.clear(); }
 }
