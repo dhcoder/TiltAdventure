f154b1c093fe4409f1435d9f333f84fcc5dc517b
David Herman <dhcoder@gmail.com>
2014-11-16 11:57:35 -0800

    New CommandScope class, which will be used for grouping commands

---
+++ dhcoder.libgdx.tool/src/dhcoder/libgdx/tool/command/CommandScope.java
@@ -0,0 +1,67 @@
+package dhcoder.libgdx.tool.command;
+
+import dhcoder.support.opt.Opt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Scope for a command, which can be used to narrow down the available commands at any given time.
+ */
+public final class CommandScope {
+    private final Opt<CommandScope> parentOpt = Opt.withNoValue();
+    private final List<CommandScope> children = new ArrayList<CommandScope>(0);
+    private final String name;
+    private final String fullName;
+
+    public CommandScope(final String name) {
+        this.name = name;
+        fullName = name;
+    }
+
+    public CommandScope(final String name, final CommandScope parentScope) {
+        parentOpt.set(parentScope);
+        this.name = name;
+        this.fullName = buildFullName();
+
+        parentScope.children.add(this);
+    }
+
+    public List<CommandScope> getChildren() {
+        return children;
+    }
+
+    public boolean isAncestorOf(final CommandScope otherScope) {
+        return (otherScope.isDescendantOf(this));
+    }
+
+    public boolean isDescendantOf(final CommandScope otherScope) {
+        CommandScope currentScope = otherScope;
+        while (currentScope.parentOpt.hasValue()) {
+            currentScope = parentOpt.getValue();
+            if (currentScope == this) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    private String buildFullName() {
+        StringBuilder fullNameBuilder = new StringBuilder(name);
+        CommandScope currentScope = this;
+        while (currentScope.parentOpt.hasValue()) {
+            currentScope = currentScope.parentOpt.getValue();
+            fullNameBuilder.insert(0, '.');
+            fullNameBuilder.insert(0, currentScope.name);
+        }
+        return fullNameBuilder.toString();
+    }
+}
---
+++ dhcoder.libgdx.tool/test/dhcoder/libgdx/tool/command/CommandScopeTest.java
@@ -0,0 +1,22 @@
+package dhcoder.libgdx.tool.command;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public final class CommandScopeTest {
+    @Test
+    public void nestedScopesHaveExpectedNames() {
+        CommandScope fileScope = new CommandScope("File");
+        CommandScope fileSettingsScope = new CommandScope("Settings", fileScope);
+        CommandScope fileSettingsMiscScope = new CommandScope("Misc", fileSettingsScope);
+
+        assertThat(fileScope.getName(), equalTo("File"));
+        assertThat(fileScope.getFullName(), equalTo("File"));
+        assertThat(fileSettingsScope.getName(), equalTo("Settings"));
+        assertThat(fileSettingsScope.getFullName(), equalTo("File.Settings"));
+        assertThat(fileSettingsMiscScope.getName(), equalTo("Misc"));
+        assertThat(fileSettingsMiscScope.getFullName(), equalTo("File.Settings.Misc"));
+    }
+}
