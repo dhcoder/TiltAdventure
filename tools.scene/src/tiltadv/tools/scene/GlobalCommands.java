2e03f8000f840ad5ed07c0fca2d4fc13ca3e3cc9
David Herman <davidherman@google.com>
2014-11-29 14:23:08 -0800

    WIP: Swing instead of libgdx scene2d

--- tools.scene/src/tiltadv/tools/scene/GlobalCommands.java
+++ tools.scene/src/tiltadv/tools/scene/GlobalCommands.java
@@ -1,92 +1,92 @@
-package tiltadv.tools.scene;
-
-import com.badlogic.gdx.Gdx;
-import dhcoder.libgdx.tool.command.Command;
-import dhcoder.libgdx.tool.command.CommandManager;
-import dhcoder.libgdx.tool.command.CommandScope;
-import dhcoder.libgdx.tool.scene2d.widget.CommandTree;
-import dhcoder.libgdx.tool.scene2d.widget.CommandWindow;
-
-/**
- * Commands that should get executed anytime.
- */
-public final class GlobalCommands {
-
-    public final CommandScope globalScope;
-    public final CommandScope helpScope;
-    public final CommandScope fileScope;
-    public final CommandScope editScope;
-    public final Command showCommandWindow;
-    public final Command newScene;
-    public final Command closeScene;
-    public final Command exit;
-    public final Command undo;
-    public final Command redo;
-    public final Command listAllCommands;
-
-    public GlobalCommands(final SceneTool sceneTool, final CommandManager commandManager) {
-        globalScope = new CommandScope("Global", true);
-        fileScope = new CommandScope("File", globalScope);
-        editScope = new CommandScope("Edit", globalScope);
-        helpScope = new CommandScope("Help", globalScope);
-
-        showCommandWindow = new Command("show_command_window", globalScope, "Show Command Window",
-            "Opens the Command Window which allows for searching all commands", new Command.RunCallback() {
-            @Override
-            public void run() {
-                final CommandWindow commandWindow = sceneTool.getCommandWindow();
-                commandWindow.show();
-            }
-        });
-        commandManager.excludeFromSearch(showCommandWindow);
-
-        newScene = new Command("new_scene", fileScope, "New Scene", "Opens a new, blank scene to work on",
-            new Command.RunCallback() {
-                @Override
-                public void run() {
-
-                }
-            });
-
-        closeScene =
-            new Command("close_scene", fileScope, "Close Scene", "Closes the current scene", new Command.RunCallback() {
-                @Override
-                public void run() {
-
-                }
-            });
-
-        exit = new Command("exit", fileScope, "Exit", "Exits the application", new Command.RunCallback() {
-            @Override
-            public void run() {
-                Gdx.app.exit();
-            }
-        });
-
-        undo = new Command("undo", editScope, "Undo", "Undo your last action", new Command.RunCallback() {
-            @Override
-            public void run() {
-
-            }
-        });
-
-        redo = new Command("redo", editScope, "Redo", "Redo your last undone action", new Command.RunCallback() {
-            @Override
-            public void run() {
-
-            }
-        });
-
-        listAllCommands =
-            new Command("toggle_command_tree", helpScope, "Show/Hide Command Tree", "Toggles the command tree window",
-                new Command.RunCallback() {
-                    @Override
-                    public void run() {
-                        CommandTree commandTree = sceneTool.getCommandTree();
-                        commandTree.setVisible(!commandTree.isVisible());
-                    }
-                });
-
-        commandManager.register(globalScope);
-    }
-}
+//package tiltadv.tools.scene;
+//
+//import com.badlogic.gdx.Gdx;
+//import dhcoder.libgdx.tool.command.Command;
+//import dhcoder.libgdx.tool.command.CommandManager;
+//import dhcoder.libgdx.tool.command.CommandScope;
+//import dhcoder.libgdx.tool.scene2d.widget.CommandTree;
+//import dhcoder.libgdx.tool.scene2d.widget.CommandWindow;
+//
+///**
+// * Commands that should get executed anytime.
+// */
+//public final class GlobalCommands {
+//
+//    public final CommandScope globalScope;
+//    public final CommandScope helpScope;
+//    public final CommandScope fileScope;
+//    public final CommandScope editScope;
+//    public final Command showCommandWindow;
+//    public final Command newScene;
+//    public final Command closeScene;
+//    public final Command exit;
+//    public final Command undo;
+//    public final Command redo;
+//    public final Command listAllCommands;
+//
+//    public GlobalCommands(final SceneTool sceneTool, final CommandManager commandManager) {
+//        globalScope = new CommandScope("Global", true);
+//        fileScope = new CommandScope("File", globalScope);
+//        editScope = new CommandScope("Edit", globalScope);
+//        helpScope = new CommandScope("Help", globalScope);
+//
+//        showCommandWindow = new Command("show_command_window", globalScope, "Show Command Window",
+//            "Opens the Command Window which allows for searching all commands", new Command.RunCallback() {
+//            @Override
+//            public void run() {
+//                final CommandWindow commandWindow = sceneTool.getCommandWindow();
+//                commandWindow.show();
+//            }
+//        });
+//        commandManager.excludeFromSearch(showCommandWindow);
+//
+//        newScene = new Command("new_scene", fileScope, "New Scene", "Opens a new, blank scene to work on",
+//            new Command.RunCallback() {
+//                @Override
+//                public void run() {
+//
+//                }
+//            });
+//
+//        closeScene =
+//            new Command("close_scene", fileScope, "Close Scene", "Closes the current scene", new Command.RunCallback() {
+//                @Override
+//                public void run() {
+//
+//                }
+//            });
+//
+//        exit = new Command("exit", fileScope, "Exit", "Exits the application", new Command.RunCallback() {
+//            @Override
+//            public void run() {
+//                Gdx.app.exit();
+//            }
+//        });
+//
+//        undo = new Command("undo", editScope, "Undo", "Undo your last action", new Command.RunCallback() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        });
+//
+//        redo = new Command("redo", editScope, "Redo", "Redo your last undone action", new Command.RunCallback() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        });
+//
+//        listAllCommands =
+//            new Command("toggle_command_tree", helpScope, "Show/Hide Command Tree", "Toggles the command tree window",
+//                new Command.RunCallback() {
+//                    @Override
+//                    public void run() {
+//                        CommandTree commandTree = sceneTool.getCommandTree();
+//                        commandTree.setVisible(!commandTree.isVisible());
+//                    }
+//                });
+//
+//        commandManager.register(globalScope);
+//    }
+//}
--- tools.scene/src/tiltadv/tools/scene/SceneTool.java
+++ tools.scene/src/tiltadv/tools/scene/SceneTool.java
@@ -1,107 +1,20 @@
 package tiltadv.tools.scene;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.utils.Json;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import dhcoder.libgdx.tool.command.CommandManager;
-import dhcoder.libgdx.tool.command.serialization.ShortcutsLoader;
-import dhcoder.libgdx.tool.scene2d.CommandListener;
-import dhcoder.libgdx.tool.scene2d.widget.CommandTree;
-import dhcoder.libgdx.tool.scene2d.widget.CommandWindow;
-import dhcoder.support.opt.Opt;
+import javax.swing.*;
 
-import static dhcoder.support.text.StringUtils.format;
+/**
+ * Main class for the scene tool. Acts as a collection of all high level UI elements and components, as well
+ * as being the entry point of the application.
+ */
+public final class SceneTool {
 
-public final class SceneTool extends ApplicationAdapter {
+    private JPanel rootPanel;
 
-    private static final String PATH_ASSETS = "ui/";
-    private static final String PATH_CONFIG = "config/scene/";
-
-    private final Opt<SceneContext> activeContextOpt = Opt.withNoValue();
-    // For debug rendering
-    private Stage stage;
-    private Skin skin;
-    private GlobalCommands globalCommands;
-    private CommandWindow commandWindow;
-    private CommandTree commandTree;
-
-    public CommandWindow getCommandWindow() {
-        return commandWindow;
-    }
-
-    public CommandTree getCommandTree() {
-        return commandTree;
-    }
-
-    public GlobalCommands getGlobalCommands() {
-        return globalCommands;
-    }
-
-    @Override
-    public void create() {
-        stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
-
-        CommandManager commandManager = new CommandManager();
-        globalCommands = new GlobalCommands(this, commandManager);
-        loadShortcutsInto(commandManager);
-
-        TextureAtlas skinAtlas = new TextureAtlas(Gdx.files.internal(PATH_ASSETS + "skin.atlas"));
-        skin = new Skin(Gdx.files.internal(PATH_ASSETS + "skin.json"), skinAtlas);
-
-        Table backgroundTable = new Table();
-        backgroundTable.setFillParent(true);
-        stage.addActor(backgroundTable);
-
-        Label firstRunLabel = new Label(format("Press {0} to open the command window",
-            globalCommands.showCommandWindow.getShortcutOpt().getValue()), skin);
-        backgroundTable.add(firstRunLabel);
-        backgroundTable.row();
-
-        Table commandWindowTable = new Table();
-        commandWindowTable.setFillParent(true);
-        stage.addActor(commandWindowTable);
-
-        commandWindow = new CommandWindow(commandManager, skin);
-        commandWindow.setVisible(false);
-        commandWindowTable.add(commandWindow).expandY().fillY().width(400f).fillX().top().pad(20f, 0f, 0f, 0f);
-
-        commandTree = new CommandTree(commandManager, skin);
-        commandTree.setVisible(false);
-        stage.addActor(commandTree);
-
-        stage.addListener(new CommandListener(globalCommands.globalScope));
-    }
-
-    @Override
-    public void resize(final int width, final int height) {
-        stage.getViewport().update(width, height, true);
-    }
-
-    @Override
-    public void render() {
-
-        Gdx.gl.glClearColor(.18f, .18f, .18f, 1f); // Classy dark gray background
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act(Gdx.graphics.getDeltaTime());
-        stage.draw();
-    }
-
-    @Override
-    public void dispose() {
-        stage.dispose();
-        skin.dispose();
-    }
-
-    private void loadShortcutsInto(final CommandManager commandManager) {
-        Json json = new Json();
-        ShortcutsLoader.load(json, commandManager, PATH_CONFIG + "shortcuts.json");
+    public static void main(final String[] args) {
+        JFrame frame = new JFrame("SceneTool");
+        frame.setContentPane(new SceneTool().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
