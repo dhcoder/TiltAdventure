0f7ca6056c4352d7fdd48ce6d396dbefb0a83bbf
David Herman <davidherman@google.com>
2014-12-23 21:39:27 -0800

    Start playing with tabs

--- tools.scene/src/tiltadv/tools/scene/view/SceneController.java
+++ tools.scene/src/tiltadv/tools/scene/view/SceneController.java
@@ -1,7 +1,26 @@
 package tiltadv.tools.scene.view;
 
+import dhcoder.libgdx.assets.Scene;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
+
 /**
  * UI for editing a scene. Contains a list of all actors in the scene, as well as a property editor and visual grid.
  */
 public final class SceneController {
+    @FXML private AnchorPane sceneGridPane;
+    @FXML private ListView listSceneItems;
+    @FXML private TabPane tabScenes;
+
+
+    public void addScene(final Scene gameScene, final String name, final EventHandler<Event> onSceneClosed) {
+        Tab tabScene = new Tab(name);
+        tabScene.setOnClosed(onSceneClosed);
+        tabScenes.getTabs().add(new Tab(name));
+    }
 }
