81d816d0a677a71b88a96d025fdbd4e111245cf2
David Herman <davidherman@google.com>
2014-12-24 21:02:17 -0800

    Resizable canvas...?

---
+++ dhcoder.tool.javafx/src/dhcoder/tool/javafx/control/ResizableCanvas.java
@@ -0,0 +1,44 @@
+package dhcoder.tool.javafx.control;
+
+import javafx.beans.InvalidationListener;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ * A canvas which can be resized dynamically.
+ * <p/>
+ * See also <a href=https://dlemmermann.wordpress.com/2014/04/10/javafx-tip-1-resizable-canvas/>Resizable Canvas tip</a>
+ */
+public final class ResizableCanvas extends Canvas {
+
+    public ResizableCanvas() {
+        // Redraw canvas when size changes.
+        InvalidationListener sizeListener = evt -> draw();
+
+        widthProperty().addListener(sizeListener);
+        heightProperty().addListener(sizeListener);
+    }
+
+    @Override
+    public boolean isResizable() {
+        return true;
+    }
+
+    @Override
+    public double prefWidth(final double height) {
+        return getWidth();
+    }
+
+    @Override
+    public double prefHeight(final double width) {
+        return getHeight();
+    }
+
+    private void draw() {
+        double width = getWidth();
+        double height = getHeight();
+
+        GraphicsContext gc = getGraphicsContext2D();
+        gc.clearRect(0, 0, width, height);
+    }
+}
