ad16184d066750625598f1573c62554887e65e57
David Herman <davidherman@google.com>
2015-01-06 21:35:42 -0800

    Mouseover tile now highlights

--- dhcoder.tool.javafx/src/dhcoder/tool/javafx/control/GridCanvas.java
+++ dhcoder.tool.javafx/src/dhcoder/tool/javafx/control/GridCanvas.java
@@ -9,14 +9,60 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
+import static dhcoder.support.text.StringUtils.format;
+
 /**
  * JavaFX control for rendering an image overlaid with a grid, furthermore allowing you to select a tile or range of
  * tiles.
  */
 public final class GridCanvas extends ResizableCanvas {
 
-    public static final int DEFAULT_GRID_WIDTH = 16;
-    public static final int DEFAULT_GRID_HEIGHT = 16;
+    public static class GridCoord {
+        private final int x;
+        private final int y;
+
+        public GridCoord(final int x, final int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = x;
+            result = 31 * result + y;
+            return result;
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) { return true; }
+            if (o == null || getClass() != o.getClass()) { return false; }
+
+            GridCoord gridCoord = (GridCoord)o;
+
+            if (x != gridCoord.x) { return false; }
+            if (y != gridCoord.y) { return false; }
+
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return format("Grid[{0}][{1}]", x, y);
+        }
+    }
+
+    public static final int DEFAULT_GRID_WIDTH = 0;
+    public static final int DEFAULT_GRID_HEIGHT = 0;
+
     private final Opt<Image> resampledImageOpt = Opt.withNoValue();
 
     private final SimpleObjectProperty<Image> image = new SimpleObjectProperty<Image>() {
@@ -54,14 +100,47 @@ public final class GridCanvas extends ResizableCanvas {
         }
     };
 
+    private int xOver;
+    private int yOver;
+
+    private boolean imageInvalidated;
+    private boolean refreshRequested;
+
+    public GridCanvas(final double width, final double height) {
+        super(width, height);
+        xOver = yOver = -1;
+        setOnMouseMoved(event -> {
+            int xOverLocal = -1;
+            int yOverLocal = -1;
+            int tileWidth = getTileWidth();
+            int tileHeight = getTileHeight();
+            if (tileWidth > 0 && tileHeight > 0) {
+                int zoomFactor = getZoomFactor();
+                xOverLocal = ((int)event.getX() / zoomFactor) / tileWidth;
+                yOverLocal = ((int)event.getY() / zoomFactor) / tileHeight;
+            }
+
+            if (xOver != xOverLocal || yOver != yOverLocal) {
+                xOver = xOverLocal;
+                yOver = yOverLocal;
+                enqueueRefresh(false);
+            }
+        });
+        setOnMouseExited(event -> {
+            xOver = yOver = -1;
+            enqueueRefresh(false);
+        });
+    }
+
+    public GridCanvas() {
+        this(0, 0);
+    }
+
     public Color getBackgroundColor() { return backgroundColor.getValue(); }
 
     public void setBackgroundColor(final Color value) { backgroundColor.set(value);}
 
     public SimpleObjectProperty<Color> backgroundColorProperty() { return backgroundColor; }
-
-    private boolean imageInvalidated;
-    private boolean refreshRequested;
 
     public Opt<Image> getImageOpt() { return Opt.ofNullable(image.getValue()); }
 
@@ -129,11 +208,19 @@ public final class GridCanvas extends ResizableCanvas {
         g.setLineWidth(1);
         int tileWidthZoomed = getTileWidth() * getZoomFactor();
         int tileHeightZoomed = getTileHeight() * getZoomFactor();
-        for (int x = 0; x <= getWidth(); x += tileWidthZoomed) {
-            g.strokeLine(x, 0, x, getHeight());
+        if (tileWidthZoomed > 0 && tileHeightZoomed > 0) {
+            for (int x = 0; x <= getWidth(); x += tileWidthZoomed) {
+                g.strokeLine(x, 0, x, getHeight());
+            }
+            for (int y = 0; y <= getHeight(); y += tileHeightZoomed) {
+                g.strokeLine(0, y, getWidth(), y);
+            }
+
+            if (xOver != -1 && yOver != -1) {
+                g.setFill(Color.grayRgb(255, 0.5));
+                g.fillRect(xOver * tileWidthZoomed, yOver * tileHeightZoomed, tileWidthZoomed, tileHeightZoomed);
+            }
         }
-        for (int y = 0; y <= getHeight(); y += tileHeightZoomed) {
-            g.strokeLine(0, y, getWidth(), y);
-        }
+
     }
 }
